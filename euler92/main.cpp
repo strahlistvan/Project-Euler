1f698518d31c60e2945722ea9b5823fb5ab84384
strahlistvan <strahlistvan0@gmail.com>
2018-01-03 20:24:25 +0100

    Optimize

--- euler92/main.cpp
+++ euler92/main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
+#include <cmath>
+#include <ctime>
 using namespace std;
 
 //is unhappy number? :'(
 bool isChainEnds89(int number);
+int getSumOfDigitSquare(int num);
 
-int main()
+int getUnhappyCount(int limit);
+int getUnhappyCountCache(int limit);
+
+int main(int argc, char ** argv)
 {
     const int LIMIT = 10000000;
-    int count = 0;
-    for (int i=2; i<LIMIT; ++i)
-    {
-        if (isChainEnds89(i))
-            ++count;
-    }
-    cout<<count<<" number chains ends 89 below "<<LIMIT<<endl;
+
+    time_t t = time(NULL);
+    cout<<getUnhappyCount(LIMIT)<<" number chains ends 89 below "<<LIMIT<<endl;
+    cout<<"Running time [NO CACHE] : " << (time(NULL) - t) <<" seconds "<< endl;
+    t = time(NULL);
+    cout<<getUnhappyCountCache(LIMIT)<<" number chains ends 89 below "<<LIMIT<<endl;
+    cout<<"Running time [CACHE] : " << (time(NULL) - t) <<" seconds "<< endl;
+
+    cout<<"\n1009 is a really "<<((isChainEnds89(1009))?"un":"")<<"happy number"<<endl;
+
     return 0;
 }
 
@@ -37,7 +46,39 @@ bool isChainEnds89(int number)
        // cout<<number<<"->";
         number = getSumOfDigitSquare(number);
     }
-    if (number==89)
-        return true;
-    return false;
+
+    return (number==89);
+}
+
+int getUnhappyCount(int limit)
+{
+    int count = 0;
+    for (int i=2; i<limit; ++i)
+    {
+        if (isChainEnds89(i))
+            ++count;
+    }
+    return count;
+}
+
+int getUnhappyCountCache(int limit)
+{
+    const int CACHE_SIZE = 81*log10(limit)+1;
+    bool cache[CACHE_SIZE] = {false};
+
+    int count = 0;
+    for (int i=2; i<CACHE_SIZE; ++i)
+    {
+        if (isChainEnds89(i))
+        {
+            ++count;
+            cache[i] = true;
+        }
+    }
+    for (int i=CACHE_SIZE; i<limit; ++i)
+    {
+        if (cache[getSumOfDigitSquare(i)])
+            ++count;
+    }
+    return count;
 }
